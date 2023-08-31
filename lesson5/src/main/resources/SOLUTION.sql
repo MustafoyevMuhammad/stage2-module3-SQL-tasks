0d6ce363ba3787d64a29caf49261ca97176d76d5
MustafoyevMuhammad <110478566+MustafoyevMuhammad@users.noreply.github.com>
2023-08-31 15:54:04 +0500

    Update SOLUTION.sql

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -4,4 +4,4 @@ SELECT * from student where groupnumber = 10 and YEAR(birthday) > 2004;
 select * from student where name like 'Mike%' or groupnumber between 4 and 6;
 SELECT * FROM Payment WHERE payment_date >= DATEADD(MONTH, -8, GETDATE());
 select * from student where name like 'A%';
-select * from student where name='Roxi%' and groupnumber = 4 or name = 'Tallie%' and groupnumber = 9;
+SELECT * FROM Student WHERE (name = 'Roxi' AND groupnumber = 4) OR (name = 'Tallie' AND groupnumber = 9);
