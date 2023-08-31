0da7e08558de41b4c18720eb8da0ccd73c66467c
MustafoyevMuhammad <110478566+MustafoyevMuhammad@users.noreply.github.com>
2023-08-31 15:48:35 +0500

    Update SOLUTION.sql

--- lesson9/src/main/resources/SOLUTION.sql
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -1,3 +1,3 @@
 select s.* from student s join mark m on s.id = m.student_id group by s.id,s.name,s.birthday,s.groupnumber having avg(m.mark) > 8;
 select s.id, s.name from student s join mark m on s.id = m.student_id group by s.id, s.name having min(m.mark) >7;
-SELECT s.id, s.name FROM University.Student s WHERE s.id IN (SELECT p.student_id FROM University.Payment p WHERE YEAR(p.payment_date) = 2019 GROUP BY p.student_id HAVING COUNT(p.id) > 2);
+SELECT s.id, s.name FROM Student s WHERE s.id IN (SELECT p.student_id FROM Payment p WHERE YEAR(p.payment_date) = 2019 GROUP BY p.student_id HAVING COUNT(p.id) > 2);
