f614a6711f2e939b82d01b15941830b6743203a5
MustafoyevMuhammad <110478566+MustafoyevMuhammad@users.noreply.github.com>
2023-09-02 09:58:14 +0500

    Update SOLUTION.sql

--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -3,5 +3,5 @@ alter table mark add check ( mark >=1 and mark <=10);
 alter table mark ALTER COLUMN student_id SET not null;
 alter table mark ALTER COLUMN subject_id SET not null;
 alter table subject add check (grade >=0 and grade <=5);
-ALTER TABLE paymenttype ADD CONSTRAINT UNIQUE (name);
-alter table payment alter column type_id set not null;
+ALTER TABLE PAYMENTTYPE ADD CONSTRAINT UNIQUE (name);
+ALTER TABLE Payment MODIFY type_id BIGINT NOT NULL, MODIFY amount DECIMAL NOT NULL, MODIFY payment_date DATETIME NOT NULL;
